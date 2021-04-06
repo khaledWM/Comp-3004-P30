fffbee947b9f31aab251c26754d992148fd8031e
jack-cameron <jack-cameron@hotmail.com>
2021-04-06 19:19:07 -0400

    created therapypage

---
+++ therapypage.cpp
@@ -0,0 +1,6 @@
+#include "therapypage.h"
+
+therapypage::therapypage()
+{
+
+}
---
+++ therapypage.h
@@ -0,0 +1,12 @@
+#ifndef THERAPYPAGE_H
+#define THERAPYPAGE_H
+
+#include <QWidget>
+
+class therapypage : public Page
+{
+public:
+    therapypage();
+};
+
+#endif // THERAPYPAGE_H
