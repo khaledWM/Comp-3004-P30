5ddd9150564d3ca858f1299e82d73d110ab9382d
tejpatel107 <tejpatel3@cmail.carleton.ca>
2021-04-06 23:09:36 -0400

    Added power level functionality

--- cpu.cpp
+++ cpu.cpp
@@ -27,6 +27,10 @@ void Cpu::startDevice(){
     connect(m.left,SIGNAL(clicked()),m.left,SLOT(leftButtonClicked()));
     connect(m.right,SIGNAL(clicked()),m.right,SLOT(rightButtonClicked()));
     connect(m.fs,SIGNAL(valueChanged(int)),m.display->getPage(4),SLOT(showValueOnDisplay(int)));
+
+//    if(m.display->getPage(1))
+    connect(m.right,SIGNAL(clicked()),m.power,SLOT(increasePower()));
+    connect(m.left,SIGNAL(clicked()),m.power,SLOT(decreasePower()));
 }
 
 
--- powerlevel.cpp
+++ powerlevel.cpp
@@ -3,10 +3,21 @@
 #include<QLabel>
 #include <QLayout>
 
-PowerLevel::PowerLevel(QWidget *parent ): QLCDNumber (parent)
+PowerLevel::PowerLevel(int p, QWidget *parent ): QLCDNumber (parent)
 {
     move(700,150);
+    power = p;
 }
 
 PowerLevel::~PowerLevel(){}
+
+void PowerLevel::increasePower()
+{
+    display(++power);
+}
+
+void PowerLevel::decreasePower()
+{
+    display(--power);
+}
 
